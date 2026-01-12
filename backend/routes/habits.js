7e53812787436a8e2e86990e5a17441a4cdc65e1
Reno-mathews <renomathews10pro@gmail.com>
2026-01-12 10:25:04 +0530

    Update

--- backend/routes/habits.js
+++ backend/routes/habits.js
@@ -6,6 +6,20 @@ const router = express.Router();
 // GET all todos
 router.get("/", async (req, res) => {
     try {
-        const habits = await Habits.find()
+        const habits = await Habits.find();
+        res.json(habits);
+    } catch(err) {
+        res.status(500).json( { message: "Error fetching todos "});
+    }
+});
+
+// POST a new todo
+router.post("/", async (req,res) => {
+    try {
+        const newHabit = new Habits({
+            title: req.body.title,
+        });
+
+        const savedHabit = await 
     }
 })
