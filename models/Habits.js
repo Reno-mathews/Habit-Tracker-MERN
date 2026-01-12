1255fb4d397ebbea48559229409a30c3bb1d1d82
Reno-mathews <renomathews10pro@gmail.com>
2026-01-12 09:48:37 +0530

    Update

--- models/Habits.js
+++ models/Habits.js
@@ -1,1 +1,21 @@
 const mongoose = require("mongoose");
+
+const habitSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    streak: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    lastCompleted: {
+        type: Date,
+        default: null,
+    },
+    createdAt: {
+        type: Date,
+        Default: Date.now
+    }
+})
